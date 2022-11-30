b69c2352de0c427ad7f401e8abf0f4cc22d812a8
quangda1 <quangda1@vingroup.net>
2022-11-30 09:59:26 +0700

    dijkstra

--- path_search.py
+++ path_search.py
@@ -20,7 +20,8 @@ def a_star(start, end, neighborMap, h):
 
     _priorityQueue.put((fscore[start],start))
     while not _priorityQueue.empty():
-        current = _priorityQueue.pop(0)
+        get_current = _priorityQueue.get()
+        current = get_current[1]
         if current == end:
             return reconstruct_path
         for neighbor in neighborMap[current]:
@@ -30,7 +31,30 @@ def a_star(start, end, neighborMap, h):
                 gScore[neighbor]= tentativeGscore
                 fScore[neighbor]= tentativeGscore + h(neighbor)
                 if neighbor not in _priorityQueue:
-                    _priorityQueue.((fscore[neighbor],neighbor))
+                    _priorityQueue.put((fscore[neighbor],neighbor))
 
     return 'Not finding path'
+
+def dijkstra(start, end, listNodes, neighborMap)
+    _priorityQueue= PriorityQueue()
+    _priorityQueue.put((0,start))
+    gScore={start: 0}
+    for node in listNodes:
+        _priorityQueue.put((10e14,node))
+    
+    while not _priorityQueue.empty():
+        get_current = _priorityQueue.get()
+        current = get_current[1]
+        priority = get_current[0]
+        if current == end:
+            if priority ==10e14:
+                return "Not found"
+            return reconstruct_path
+        for neighbor in neighborMap[current]:
+            tentativeGscore = gScore[current] + d(current, neighbor)
+            if (tentativeGscore< gScore[neighbor]):
+                map[current]= neighbor
+                gScore[neighbor]= tentativeGscore
+            
+
         
